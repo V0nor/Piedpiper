d47e805c157ec0d7bb812a643dbb7c6eac216e64
Victor de Carli <88126005+V0nor@users.noreply.github.com>
2021-09-16 18:24:21 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -6,4 +6,11 @@ btn.addEventListener('click', function() {
     if (container.style.display === 'none') {} else {
         container.style.display = 'block';
     }
+    $('html, body').animate({
+        scrollTop: targetOffset - 100
+    }, 500);
 });
+
+function onOver(section) {
+    elemento.style.backgroundColor = 'red';
+}
