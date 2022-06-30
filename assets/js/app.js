b0908e962ab860001b3ee2117f47b289e2f72229
V0nor <vonorvictor@gmail.com>
2022-06-30 10:41:59 -0400

    m

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,6 @@
-var btn = document.getElementById('btn');
-var container = document.querySelector('.bg-season1');
+const btn = document.querySelector('.internet__btn');
+const container = document.querySelector('.bg-season1');
+btn.addEventListener('click', () => {
+    container.classList.toggle('active');
 
-btn.addEventListener('click', function() {
-
-    var transition, t;
-    clearInterval(transition);
-
-    if (container.style.display == 'none') {
-        container.style.opacity = "0";
-        container.style.display = "block";
-        var x = .1;
-    } else {
-        var x = .9;
-        t = true;
-
-    }
-
-    transition = setInterval(function() {
-        container.style.opacity = x;
-        x += t ? -.1 : .1;
-        if ((x >= 1 && !t) || (x <= 0 && t)) {
-            clearInterval(transition);
-            if (x <= 0) container.style.display = 'none';
-        }
-    }, 50);
-
-
-});
+})
