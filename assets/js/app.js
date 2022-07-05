e01c1752d0707d6aaffcf3087187aa3fdea035da
V0nor <vonorvictor@gmail.com>
2022-07-05 17:03:34 -0400

    Delete app.js

--- assets/js/app.js
+++
@@ -1,9 +0,0 @@
-const menuMobile = document.querySelector('.navbar__toggle');
-const navbarLinks = document.querySelector('.navbar__links');
-
-const header = document.querySelector('.header');
-
-menuMobile.addEventListener('click', () => {
-    navbarLinks.classList.toggle('show__links');;
-    menuMobile.classList.toggle('active');
-});
